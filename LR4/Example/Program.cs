c42888196ffc0ada41bbf3ecd92472cecaa90354
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-10-08 21:10:07 +0300

    Add files via upload

---
+++ LR4/Example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+   struct Lud {
+      public string f;
+      public int gr;
+      public char p;
+
+      public string Vivod() {
+         string s = f + " " + Convert.ToString(gr) + " " + Convert.ToString(p);
+         return s;
+      }
+   }
+
+   static void Main(string[] args) {
+      List<Lud> students = new List<Lud>();
+      Lud stud;
+      Lud mx = new Lud();
+      
+      int n = 4;
+      
+      Console.WriteLine("Введите данные: ");
+
+      for (int i = 0; i < n; i++) {
+         stud.f = Console.ReadLine();
+         stud.gr = Convert.ToInt32(Console.ReadLine());
+         stud.p = Console.ReadLine()[0];  
+         students.Add(stud);
+      }
+
+      for (int i = 0; i < n; i++) {
+         Console.WriteLine(students[i].Vivod());
+      }
+
+      for (int i = 0; i < n; i++) {
+         if (students[i].p == 'ж' && (students[i].gr == 0 || students[i].gr > mx.gr)) {
+            mx = students[i];
+         }
+      }
+
+      if (mx.gr != 0) {
+         Console.WriteLine("\nСамая молодая студентка: {0} {1} {2}", mx.f, mx.gr, mx.p);
+      } else {
+         Console.WriteLine("\nНет девушек");
+      }
+
+      Console.ReadKey();
+   }
+}
